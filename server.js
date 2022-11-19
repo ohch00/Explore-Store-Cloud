f547d1b3d5f0a8a5a260f7ef5115bdf5fc8156b8
ohch00 <christina.oh.y@gmail.com>
2022-11-18 20:48:55 -0800

    write model, controller functions for User entity

--- server.js
+++ server.js
@@ -69,17 +69,46 @@ const checkJwt = jwt({
 
 /* ------------- Begin User Model Functions ------------- */
 
+async function add_user(name){
+    var key = datastore.key(USER);
+	const new_user = {"name": name};
+	return await datastore.save({"key": key, "data": new_user}).then(() => {return key});
+}
+
+async function get_all_users(){
+    const q = datastore.createQuery(USER);
+    return await datastore.runQuery(q)
+    .then( (entities) => {
+            return entities[0].map(fromDatastore)
+    });
+}
+
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin User Controller Functions ------------- */
 
 // route customization from https://github.com/auth0/express-openid-connect/blob/master/EXAMPLES.md#3-route-customization
-app.get('/login', function(req, res){
+router.get('/login', function(req, res){
     res.oidc.login({ returnTo: '/user' });
 });
 
-app.get('/user', requiresAuth(), function(req, res){
+router.get('/user', requiresAuth(), function(req, res){
     res.send("JWT Token: " + req.oidc.idToken);
+});
+
+router.get('/', function(req, res){
+    if (!check_header_type(req)){
+        res.status(406).json({
+            "Error": "Not Acceptable"
+        });
+        return;
+    } else {
+        get_all_users()
+        .then( (users) => {
+            res.status(200).json(users);
+            return;
+        });
+    }
 });
 
 /* ------------- End Controller Functions ------------- */
@@ -113,6 +142,20 @@ app.get('/user', requiresAuth(), function(req, res){
 /* ------------- Begin Relationship Controller Functions ------------- */
 
 /* ------------- End Controller Functions ------------- */
+
+/* ------------- Begin Helper Functions ------------- */
+
+// 406 - Accept Header is not JSON
+function check_header_type(req){
+    if(req.get('accept') !== 'application/json'){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/* ------------- End Helper Functions ------------- */
+
 
 
 app.use('/', router);
