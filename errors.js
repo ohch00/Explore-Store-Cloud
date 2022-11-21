80a65f861db97ae885700652d958af53175703d1
ohch00 <christina.oh.y@gmail.com>
2022-11-21 13:59:33 -0800

    add error messages

--- errors.js
+++ errors.js
@@ -5,6 +5,8 @@ module.exports = {
     "403_owner": "This store is owned by another user",
     "403_name": "The requested name attribute is already assigned to another store",
     "403_owner_and_name": "The requested name attribute already assigned to another store or is owned by another user",
+    "403_already_stocked": "This store is owned by another user or the product is already stocked at this store",
+    "403_no_stock": "This store is owned by another user or the product is not at this store",
     "404_store": "No store with this store_id exists",
     "404_product": "No product with this product_id exists",
     "405_edit": "The functionality to edit all entities at once is not supported",
